009e5aa7af46ddbbd2c8106778e0ef8230a43bde
xiaohoutongxue <934163168@qq.com>
2019-05-09 15:50:03 +0800

    提交

---
+++ src/main/java/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * //TODO
+ *
+ * @author Hou jianhao
+ * @version 1.0
+ * @projectname hello
+ * @classname Hello
+ * @date 2019/5/9 15:00
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
